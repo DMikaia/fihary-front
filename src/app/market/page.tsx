18e21fd2cc77c4bbd4eb2629c63ac7b35ab6e009
KiritoEM <johankirito64@gmail.com>
2024-11-30 02:27:41 +0300

    feat: add header to market

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,11 +1,21 @@
 "use client"
 
+import Image from "next/image";
 import { FC } from "react";
 
 const MarketPlace: FC = (): JSX.Element => {
     return (
-        <section className="">
-
+        <section className="marketplace__content mt-[21vh]">
+            <div className="container mx-auto px-10">
+                <header className="flex flex-col gap-5">
+                    <div className="markdown flex items-center gap-3">
+                        <Image src="/icons/home-markdown.svg" alt="home-icon" height={23} width={23} />
+                        <p className="text-[#6E6E6E]">Marketplace /</p>
+                    </div>
+                    <h3 className="text-[#A1D83B] inter-tight text-4xl">Marketplace</h3>
+                    <p className="text-secondary-2">Trouvez des produits locaux, frais, et abordable à portée de clic</p>
+                </header>
+            </div>
         </section>
     );
 };
