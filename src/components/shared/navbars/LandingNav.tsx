cb07261ef781cd9ec6a19b12763d58a5bd93ebc9
DMikaia <faeldan03@gmail.com>
2024-11-29 13:10:06 +0300

    feat: added the basic structure of the landing nav

---
+++ src/components/shared/navbars/LandingNav.tsx
@@ -0,0 +1,7 @@
+export default function LandingNav() {
+  return (
+    <nav className="landing__nav w-full overflow-x-hidden px-6 md:px-8 py-8">
+      <div className="container mx-auto w-full"></div>
+    </nav>
+  );
+}
