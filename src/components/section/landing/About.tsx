4a3d1bb7e35cf3e4e66227939e6009382068ccac
KiritoEM <johankirito64@gmail.com>
2024-11-29 19:56:12 +0300

    feat: add about -> landing

--- src/components/section/landing/About.tsx
+++ src/components/section/landing/About.tsx
@@ -9,16 +9,19 @@ const About: FC = (): JSX.Element => {
         <section className="landing__about w-full overflow-hidde h-screen mt-16">
             <div className="container mx-auto px-7 bg-white shadow-boxShadow1 rounded-lg">
                 <div className="content px-10 py-8">
-                    <header className="flex justify-between items-center">
+                    <header className="flex justify-between items-end">
                         <div>
                             <p className="text-primary-foreground">NOS PRODUITS</p>
-                            <h2 className="text-secondary text-3xl inter-tight max-w-[400px] mt-5"><b>Explorez notre </b>sélection de produits</h2>
+                            <h2 className="text-secondary text-3xl inter-tight max-w-[300px] mt-5"><b>Explorez notre </b>sélection de produits</h2>
                         </div>
                         <Button variant="ghost" className="text-secondary">
                             Afficher plus
                             <Image alt="arrow-up-svg" height={23} width={23} src="/icons/arrow-up.svg" />
                         </Button>
                     </header>
+                    <div className="products-list">
+                        
+                    </div>
                 </div>
             </div>
         </section>
---
+++ src/components/shared/cards/LandingProductCard.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const LandingProductCard: FC = (): JSX.Element => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LandingProductCard;
