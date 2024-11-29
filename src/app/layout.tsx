25e3426b7141f60f99a2e938fca8f1824ad8d786
DMikaia <faeldan03@gmail.com>
2024-11-29 12:20:04 +0300

    config: added the work sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "../providers/ThemeProvider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const workSans = localFont({
+  src: "./fonts/WorkSans.ttf",
+  variable: "--font-work-sans",
   weight: "100 900",
 });
 
@@ -26,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${workSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
