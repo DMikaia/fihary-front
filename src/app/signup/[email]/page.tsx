7b27f90d909dcdcd802a18207819e3f83994ccae
KiritoEM <johankirito64@gmail.com>
2024-11-30 11:28:19 +0300

    feat: add dark-mode to signup

--- src/app/signup/[email]/page.tsx
+++ src/app/signup/[email]/page.tsx
@@ -4,18 +4,36 @@ import "@dotlottie/react-player/dist/index.css";
 import { useParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { DotLottiePlayer } from "@dotlottie/react-player";
 import "@dotlottie/react-player/dist/index.css";
+import { useTheme } from "next-themes";
 
 // email-lotties.lottie
 const SendCode: FC = (): JSX.Element => {
     const email = decodeURIComponent(useParams().email as string);
+    const [mounted, setMounted] = useState(false);
+    const { theme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return <></>;
+    }
+
     return (
         <div className="signup__content max-w-[410px] md:max-w-[420px] lg:max-w-[420px] w-full flex flex-col gap-5 items-center mt-10 mb-10 px-6 md:px-0 relative z-30">
-            <Image alt="fihary-logo" height={100} width={100} src="/icons/logo.svg" />
-            <h2 className="inter-tight text-2xl md:text-3xl text-center text-secondary">Consultez votre email</h2>
-            <p className="text-secondary-2 text-center">Un email de vérification a été envoyé à l’adresse email <b className="text-primary-foreground">{email}</b>. Veuillez vérifiez vérifier votre boite de réception</p>
+            <Image
+                src={theme === "dark" ? "/icons/logo-dark.svg" : "/icons/logo-2.svg"}
+                width={100}
+                height={100}
+                alt="Fihary logo"
+                className="w-[96px]"
+            />
+            <h2 className="inter-tight text-2xl md:text-3xl text-center text-secondary dark:text-white">Consultez votre email</h2>
+            <p className="text-secondary-2 text-center dark:text-darkGray">Un email de vérification a été envoyé à l’adresse email <b className="text-primary-foreground">{email}</b>. Veuillez vérifiez vérifier votre boite de réception</p>
             <DotLottiePlayer
                 autoplay={true}
                 loop={true}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,17 +2,34 @@
 
 import SignupForm from "@/components/shared/forms/SignupForm";
 import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 const Signup: FC = (): JSX.Element => {
+    const [mounted, setMounted] = useState(false);
+    const { theme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return <></>;
+    }
     return (
         <div className="signup__content max-w-[410px] md:max-w-[420px] lg:max-w-[420px] w-full flex flex-col gap-5 items-center mt-10 mb-10 px-6 md:px-0 relative z-30">
-            <Image alt="fihary-logo" height={100} width={100} src="/icons/logo.svg" />
-            <h2 className="inter-tight text-2xl md:text-3xl text-center text-secondary">Prêt a démarrer sur <b>Fihary</b> ?
+            <Image
+                src={theme === "dark" ? "/icons/logo-dark.svg" : "/icons/logo-2.svg"}
+                width={100}
+                height={100}
+                alt="Fihary logo"
+                className="w-[96px]"
+            />
+            <h2 className="inter-tight text-2xl md:text-3xl text-center text-secondary dark:text-white">Prêt a démarrer sur <b>Fihary</b> ?
                 Créér votre compte maintenant</h2>
-            <p className="text-secondary-2 text-center max-w-[90%]">Entrez vos informations ci-dessous pour commencer</p>
+            <p className="text-secondary-2 text-center max-w-[90%] dark:text-darkGray">Entrez vos informations ci-dessous pour commencer</p>
             <SignupForm />
             <div className="or flex items-center gap-3 w-full">
                 <div className="line flex-1 h-[1px] bg-input"></div>
@@ -27,12 +44,12 @@ const Signup: FC = (): JSX.Element => {
                     width={24}
                     height={24}
                 />
-                <p className="text-secondary text-base">
+                <p className="text-secondary dark:text-white text-base">
                     Se connecter avec Google
                 </p>
             </Button>
 
-            <p className="w-full text-center text-secondary">
+            <p className="w-full text-center text-secondary dark:text-white">
                 Pas encore de compte?{" "}
                 <Link
                     href="/signup"
