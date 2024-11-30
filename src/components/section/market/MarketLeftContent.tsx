ec6d4dc4168dfe523c7c3173aafe1ce1c205b290
DMikaia <faeldan03@gmail.com>
2024-11-30 07:23:36 +0300

    fix: changed some responsive style of components

--- src/components/section/market/MarketLeftContent.tsx
+++ src/components/section/market/MarketLeftContent.tsx
@@ -6,15 +6,18 @@ import Cart from "./components/Cart";
 
 const MarketLeftContent: FC = (): JSX.Element => {
   return (
-    <div className="market-left w-[280px]">
+    <div className="market__left w-full md:w-[280px]">
       <header className="flex items-center gap-4">
         <h3 className="inter-tight text-2xl text-secondary">Filtres</h3>
         <span className="text-secondary-2 underline">Effacer</span>
       </header>
+
       <MarketCategories />
+
       <div className="prices mt-5">
         <h3 className="text-lg">Price(5K-100K)</h3>
       </div>
+
       <Cart />
     </div>
   );
