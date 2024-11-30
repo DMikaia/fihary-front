968d7dcffe6f757ea80b908703675209a8171172
DMikaia <faeldan03@gmail.com>
2024-11-30 15:02:19 +0300

    fix: fixed the responsive design of the dashboard

--- src/components/section/admin/dashboard/DashboardRight.tsx
+++ src/components/section/admin/dashboard/DashboardRight.tsx
@@ -3,7 +3,7 @@ import { GeneralChart } from "./components/Radar";
 
 export default function DashboardRight() {
   return (
-    <div className="w-full lg:-[30%] flex flex-col md:flex-row lg:flex-col gap-8">
+    <div className="w-full lg:-[30%] flex-col hidden lg:flex gap-8">
       <div className="w-full h-[322px]">
         <AdminNotification />
       </div>
