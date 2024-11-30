720604bf9e526159d1df417769237bcfb43e643e
KiritoEM <johankirito64@gmail.com>
2024-11-30 03:03:45 +0300

    feat: add cart for marketplace

--- src/components/section/market/MarketLeftContent.tsx
+++ src/components/section/market/MarketLeftContent.tsx
@@ -2,10 +2,11 @@
 
 import { FC } from "react";
 import MarketCategories from "./components/MarketCategories";
+import Cart from "./components/Cart";
 
 const MarketLeftContent: FC = (): JSX.Element => {
     return (
-        <div className="market-left w-[450px]">
+        <div className="market-left w-[250px]">
             <header className="flex items-center gap-4"><h3 className="inter-tight text-2xl text-secondary">Filtres</h3>
                 <span className="text-secondary-2 underline">Effacer</span>
             </header>
@@ -13,6 +14,7 @@ const MarketLeftContent: FC = (): JSX.Element => {
             <div className="prices mt-5">
                 <h3 className="text-lg">Price(5K-100K)</h3>
             </div>
+            <Cart />
         </div>
     );
 };
--- src/components/section/market/components/Cart.tsx
+++ src/components/section/market/components/Cart.tsx
@@ -1,2 +1,32 @@
 "use client"
 
+import React, { FC } from 'react';
+import { DotLottiePlayer } from "@dotlottie/react-player";
+import "@dotlottie/react-player/dist/index.css";
+import { Button } from '@/components/ui/button';
+
+const Cart: FC = (): JSX.Element => {
+    return (
+        <div className="cart mt-14">
+            <header className="flex items-center gap-4">
+                <h3 className="text-lg">Votre panier</h3>
+                <span className="text-secondary-2 underline">Effacer</span>
+            </header>
+            <div className="mt-8 flex flex-col gap-3">
+                {/* cart-lottie.lottie */}
+                <DotLottiePlayer
+                    autoplay={true}
+                    loop={true}
+                    src="/lotties/cart-lottie.lottie"
+                    style={{ width: "100%", height: "7em", objectFit: "cover" }}
+                />
+                <p className="text-center text-sm text-secondary-2">Votre panier est vide</p>
+            </div>
+            <Button className="bg-[#9E9E9E] w-full mt-10">
+                Confirmer
+            </Button>
+        </div>
+    );
+};
+
+export default Cart;
